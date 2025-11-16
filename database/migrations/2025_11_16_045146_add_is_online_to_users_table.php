bf116a08b65c1f8669f7d25f9f5233109cb5d836
novatrrustbank <collaomn@gmail.com>
2025-11-16 06:33:40 +0100

    git commit -m "Add is_online column to users"

--- database/migrations/2025_11_16_045146_add_is_online_to_users_table.php
+++ database/migrations/2025_11_16_045146_add_is_online_to_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_online')->default(false);
+            $table->boolean('is_online')->default(true);
         });
     }
 

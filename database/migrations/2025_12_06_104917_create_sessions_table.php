3720e802c74211a2c71ca65959a312da9869279b
novatrrustbank <collaomn@gmail.com>
2026-04-25 14:09:37 +0100

    Refactor session migration to use modern syntax

--- database/migrations/2025_12_06_104917_create_sessions_table.php
+++ database/migrations/2025_12_06_104917_create_sessions_table.php
@@ -1,14 +1,8 @@
-<?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('sessions', function (Blueprint $table) {
@@ -21,9 +15,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('sessions');

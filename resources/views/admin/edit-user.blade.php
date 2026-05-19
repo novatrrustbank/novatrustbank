99608016e932b3f405c0a0e81c12b4eb895cccae
novatrrustbank <collaomn@gmail.com>
2026-05-19 18:57:47 +0100

    Update edit-user.blade.php

--- resources/views/admin/edit-user.blade.php
+++ resources/views/admin/edit-user.blade.php
@@ -5,32 +5,70 @@
 
     <h2>Edit User: {{ $user->name }}</h2>
 
-    <form action="{{ route('admin.updateUser') }}" method="POST">
+    <form action="{{ route('admin.updateUser') }}"
+          method="POST"
+          enctype="multipart/form-data">
         @csrf
 
         <input type="hidden" name="user_id" value="{{ $user->id }}">
 
         <div class="mb-3">
             <label>Name</label>
-            <input type="text" name="name" value="{{ $user->name }}" class="form-control">
+            <input type="text"
+                   name="name"
+                   value="{{ $user->name }}"
+                   class="form-control">
         </div>
 
         <div class="mb-3">
             <label>Email</label>
-            <input type="email" name="email" value="{{ $user->email }}" class="form-control">
+            <input type="email"
+                   name="email"
+                   value="{{ $user->email }}"
+                   class="form-control">
         </div>
 
         <div class="mb-3">
             <label>Balance</label>
-            <input type="number" step="0.01" name="balance" value="{{ $user->balance }}" class="form-control">
+            <input type="number"
+                   step="0.01"
+                   name="balance"
+                   value="{{ $user->balance }}"
+                   class="form-control">
         </div>
 
         <div class="mb-3">
             <label>New Password (optional)</label>
-            <input type="password" name="password" class="form-control">
+            <input type="password"
+                   name="password"
+                   class="form-control">
         </div>
 
-        <button class="btn btn-primary">Update</button>
+        <!-- PASSPORT PHOTO -->
+        <div class="mb-3">
+            <label>Passport Photo</label>
+
+            <input type="file"
+                   name="passport_photo"
+                   class="form-control">
+
+            @if($user->passport_photo)
+                <img src="{{ asset('storage/' . $user->passport_photo) }}"
+                     alt="Passport"
+                     style="
+                        width:100px;
+                        height:100px;
+                        object-fit:cover;
+                        border-radius:50%;
+                        margin-top:10px;
+                        border:2px solid #1a237e;
+                     ">
+            @endif
+        </div>
+
+        <button class="btn btn-primary">
+            Update
+        </button>
 
     </form>
 

a7acd64a4bcec0e2d2ccb0336bb36fd45e3aeac1
novatrrustbank <collaomn@gmail.com>
2026-09-04 23:50:44 +0100

    Add dashboard messages in Portuguese language file

--- resources/lang/pt/messages.php
+++ resources/lang/pt/messages.php
@@ -2,16 +2,26 @@
 
 return [
 
+    // LOGIN
     'simple_secure_banking' => 'Sistema bancário simples, rápido e seguro',
-
     'send_funds' => 'Envie fundos para mais de 130 países em poucos minutos',
-
     'login' => 'Entrar',
-
     'email_address' => 'Endereço de e-mail',
-
     'password' => 'Palavra-passe',
-
     'forgot_password' => 'Esqueceu a palavra-passe?',
+
+    // DASHBOARD
+    'welcome_back' => 'Bem-vindo de volta',
+    'passport' => 'Passaporte',
+    'account_number' => 'Número da conta',
+    'current_balance' => 'Saldo atual',
+    'view_transfer_instruction' => 'Ver instruções de transferência',
+    'make_transfer' => 'Fazer transferência',
+    'view_history' => 'Ver histórico',
+    'direct_chat' => 'Chat direto',
+    'contact_novatrust_bank' => 'Contactar a NovaTrust Bank',
+    'washington_dc_usa' => 'Washington DC, EUA',
+    'tel' => 'Tel',
+    'all_rights_reserved' => 'Todos os direitos reservados',
 
 ];

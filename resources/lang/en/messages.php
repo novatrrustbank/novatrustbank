b921a6dae475ca823072df9039a9a9d2f5a7fdc1
novatrrustbank <collaomn@gmail.com>
2026-09-04 22:11:02 +0100

    Add English language messages for banking system

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,1 +1,17 @@
+<?php
 
+return [
+
+    'simple_secure_banking' => 'Simple, Quick, Secure Banking System',
+
+    'send_funds' => 'Send Funds From Us To 130+ Countries Within Minutes',
+
+    'login' => 'Login',
+
+    'email_address' => 'Email Address',
+
+    'password' => 'Password',
+
+    'forgot_password' => 'Forgot Password?',
+
+];

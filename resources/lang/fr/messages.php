29e6bf48c613cb54c50dc8e74c51d6d82acf39e7
novatrrustbank <collaomn@gmail.com>
2026-09-04 22:47:48 +0100

    Add French translation for banking messages

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'simple_secure_banking' => 'Système bancaire simple, rapide et sécurisé',
+
+    'send_funds' => 'Envoyez des fonds vers plus de 130 pays en quelques minutes',
+
+    'login' => 'Connexion',
+
+    'email_address' => 'Adresse e-mail',
+
+    'password' => 'Mot de passe',
+
+    'forgot_password' => 'Mot de passe oublié ?',
+
+];

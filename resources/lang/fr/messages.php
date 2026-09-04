819108c08de662b2aeacb3d3ee9926c45fc4d404
novatrrustbank <collaomn@gmail.com>
2026-09-04 23:47:07 +0100

    Add French translations for dashboard messages

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -2,16 +2,26 @@
 
 return [
 
+    // LOGIN
     'simple_secure_banking' => 'Système bancaire simple, rapide et sécurisé',
-
     'send_funds' => 'Envoyez des fonds vers plus de 130 pays en quelques minutes',
-
     'login' => 'Connexion',
-
     'email_address' => 'Adresse e-mail',
-
     'password' => 'Mot de passe',
-
     'forgot_password' => 'Mot de passe oublié ?',
+
+    // DASHBOARD
+    'welcome_back' => 'Bon retour',
+    'passport' => 'Passeport',
+    'account_number' => 'Numéro de compte',
+    'current_balance' => 'Solde actuel',
+    'view_transfer_instruction' => 'Voir les instructions de virement',
+    'make_transfer' => 'Effectuer un virement',
+    'view_history' => 'Voir l’historique',
+    'direct_chat' => 'Chat direct',
+    'contact_novatrust_bank' => 'Contacter NovaTrust Bank',
+    'washington_dc_usa' => 'Washington DC, États-Unis',
+    'tel' => 'Tél',
+    'all_rights_reserved' => 'Tous droits réservés',
 
 ];

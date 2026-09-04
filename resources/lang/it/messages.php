6ec902fb7a2299c997c20991368ec772b2aae34d
novatrrustbank <collaomn@gmail.com>
2026-09-04 23:08:51 +0100

    Add Italian language messages for the application

---
+++ resources/lang/it/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'simple_secure_banking' => 'Sistema bancario semplice, veloce e sicuro',
+
+    'send_funds' => 'Invia fondi in oltre 130 paesi in pochi minuti',
+
+    'login' => 'Accedi',
+
+    'email_address' => 'Indirizzo email',
+
+    'password' => 'Password',
+
+    'forgot_password' => 'Password dimenticata?',
+
+];

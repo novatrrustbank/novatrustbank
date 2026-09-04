e6d5d7f83dff1ed03cfc20cd43d8be11a4096841
novatrrustbank <collaomn@gmail.com>
2026-09-04 20:33:24 +0100

    Update TransferController.php

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -131,30 +131,7 @@ class TransferController extends Controller
     }
 
 
-    // ==============================
-    // SHOW TAC PAGE
-    // ==============================
-
-    public function showTac()
-    {
-
-        if (!session()->has('transfer')) {
-
-            return redirect()
-                ->route('transfer.form')
-                ->with(
-                    'error',
-                    'No transfer transaction was found.'
-                );
-
-        }
-
-
-        return view('tac');
-
-    }
-
-public function verifyTac(Request $request)
+  public function verifyTac(Request $request)
 {
 // ==============================
 // VALIDATE TAC INPUT

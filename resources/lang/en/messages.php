8b92398c6b8c1d41447a974d9420aac520725e29
novatrrustbank <collaomn@gmail.com>
2026-09-05 00:27:12 +0100

    Add transfer-related language strings

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -24,4 +24,25 @@ return [
     'tel' => 'Tel',
     'all_rights_reserved' => 'All Rights Reserved',
 
+     // TRANSFER
+    'transfer_funds' => 'Transfer Funds',
+    'processing_transfer' => 'Processing Transfer...',
+    'dashboard' => 'Dashboard',
+    'transfer' => 'Transfer',
+    'history' => 'History',
+    'logout' => 'Logout',
+    'enter_recipient_account_number' => 'Enter recipient account number',
+    'account_name' => 'Account Name',
+    'enter_account_name' => 'Enter account name',
+    'select_bank' => 'Select Bank',
+    'other_not_listed' => 'Other (Not Listed)',
+    'enter_bank_name' => 'Enter Bank Name',
+    'enter_your_bank_name' => 'Enter your bank name',
+    'amount' => 'Amount',
+    'enter_amount_to_transfer' => 'Enter amount to transfer',
+    'send_money' => 'Send Money',
+    'please_enter_bank_name' => 'Please enter your bank name.',
+    'processing' => 'Processing...',
+    'chat' => 'Chat',
+
 ];

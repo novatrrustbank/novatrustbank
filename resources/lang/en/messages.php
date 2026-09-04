4f444a4f5eb8cd78e6a78c71be73663fa4f048a0
novatrrustbank <collaomn@gmail.com>
2026-09-04 23:45:28 +0100

    Add dashboard messages to English language file

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,16 +2,26 @@
 
 return [
 
+    // LOGIN
     'simple_secure_banking' => 'Simple, Quick, Secure Banking System',
-
     'send_funds' => 'Send Funds From Us To 130+ Countries Within Minutes',
-
     'login' => 'Login',
-
     'email_address' => 'Email Address',
-
     'password' => 'Password',
-
     'forgot_password' => 'Forgot Password?',
+
+    // DASHBOARD
+    'welcome_back' => 'Welcome back',
+    'passport' => 'Passport',
+    'account_number' => 'Account Number',
+    'current_balance' => 'Current Balance',
+    'view_transfer_instruction' => 'View Transfer Instruction',
+    'make_transfer' => 'Make Transfer',
+    'view_history' => 'View History',
+    'direct_chat' => 'Direct Chat',
+    'contact_novatrust_bank' => 'Contact NovaTrust Bank',
+    'washington_dc_usa' => 'Washington DC, USA',
+    'tel' => 'Tel',
+    'all_rights_reserved' => 'All Rights Reserved',
 
 ];

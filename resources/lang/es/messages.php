ea6e0d9afa95f7161573c012bffd254f363aa965
novatrrustbank <collaomn@gmail.com>
2026-09-04 23:00:08 +0100

    Add Spanish language translations for messages

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'simple_secure_banking' => 'Sistema bancario simple, rápido y seguro',
+
+    'send_funds' => 'Envía fondos a más de 130 países en cuestión de minutos',
+
+    'login' => 'Iniciar sesión',
+
+    'email_address' => 'Dirección de correo electrónico',
+
+    'password' => 'Contraseña',
+
+    'forgot_password' => '¿Olvidaste tu contraseña?',
+
+];

59c5a9df03e23a44625d14cb28cac4aa57d30bd9
novatrrustbank <collaomn@gmail.com>
2026-09-08 15:36:50 +0100

    Create NewAccountCreated Mailable for user accounts

---
+++ app/Mail/NewAccountCreated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewAccountCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        return $this->subject('Welcome to NovaTrust Bank - Your Account Has Been Created')
+                    ->view('emails.new-account-created');
+    }
+}
